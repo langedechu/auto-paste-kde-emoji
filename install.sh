9dae205b94c82749b7d308136719f15f9e3b63f1
Paul BONCAR <contact.nocteria@gmail.com>
2023-11-06 17:07:53 +0100

    Copy the file from installation folder instead of the downloaded one.

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ bun install
 mkdir ~/.auto-paste-kde-emoji
 cp -r ./** ~/.auto-paste-kde-emoji
 
-sudo ln ./auto-paste-kde-emoji.sh /usr/local/bin/auto-paste-kde-emoji
+sudo ln ~/.auto-paste-kde-emoji/auto-paste-kde-emoji.sh /usr/local/bin/auto-paste-kde-emoji
 sudo chmod +x /usr/local/bin/auto-paste-kde-emoji

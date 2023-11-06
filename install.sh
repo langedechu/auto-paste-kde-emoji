78c727921f76cf0bb00a8d0fc0a9135285ade536
Paul BONCAR <contact.nocteria@gmail.com>
2023-11-06 16:55:42 +0100

    Create install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+mkdir ~/.auto-paste-kde-emoji
+cp -r ./** ~/.auto-paste-kde-emoji
+
+sudo ln ./auto-paste-kde-emoji.sh /usr/local/bin/auto-paste-kde-emoji
+sudo chmod +x /usr/local/bin/auto-paste-kde-emoji
